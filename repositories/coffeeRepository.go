4f32c4c66b71bae766bf66581fab64e29b24cd63
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-04-12 01:04:10 +0530

    adds coffee service and repository

---
+++ repositories/coffeeRepository.go
@@ -0,0 +1,51 @@
+package repositories
+
+import (
+	"github.com/2k4sm/httpCoffee/entities"
+	"gorm.io/gorm"
+)
+
+type CoffeeRepositoryInterface interface {
+	FindAll() []entities.Coffee
+	FindById(id uint) entities.Coffee
+	FindByName(coffeeName string) entities.Coffee
+	Save(newCoffee entities.Coffee) entities.Coffee
+	Delete(coffee entities.Coffee)
+}
+
+type coffeeRepository struct {
+	Db *gorm.DB
+}
+
+func NewCoffeeRepository(Db *gorm.DB) CoffeeRepositoryInterface {
+	return &coffeeRepository{
+		Db: Db,
+	}
+}
+
+func (c *coffeeRepository) FindAll() []entities.Coffee {
+	var coffees []entities.Coffee
+	c.Db.Find(&coffees)
+	return coffees
+}
+
+func (c *coffeeRepository) FindById(id uint) entities.Coffee {
+	var coffee entities.Coffee
+	c.Db.First(&coffee, id)
+	return coffee
+}
+
+func (c *coffeeRepository) FindByName(coffeeName string) entities.Coffee {
+	var coffee entities.Coffee
+	c.Db.First(&coffee, "name = ?", coffeeName)
+	return coffee
+}
+
+func (c *coffeeRepository) Save(newCoffee entities.Coffee) entities.Coffee {
+	c.Db.Save(&newCoffee)
+	return newCoffee
+}
+
+func (c *coffeeRepository) Delete(coffee entities.Coffee) {
+	c.Db.Delete(&coffee)
+}
---
+++ services/coffeeService.go
@@ -0,0 +1,62 @@
+package services
+
+import (
+	"github.com/2k4sm/httpCoffee/dto"
+	"github.com/2k4sm/httpCoffee/entities"
+	"github.com/2k4sm/httpCoffee/repositories"
+	"gorm.io/gorm"
+)
+
+type CoffeeServiceInterface interface {
+	GetCoffees() []entities.Coffee
+	GetCoffeeById(id int) entities.Coffee
+	GetCoffeeByName(coffeeName string) entities.Coffee
+	CreateNewCoffee(newCoffee dto.CreateCoffee) entities.Coffee
+	DeleteCoffee(id int) error
+}
+
+type coffeeService struct {
+	CoffeeRepository repositories.CoffeeRepositoryInterface
+}
+
+func NewCoffeeService(coffeeRepository repositories.CoffeeRepositoryInterface) CoffeeServiceInterface {
+	return &coffeeService{
+		CoffeeRepository: coffeeRepository,
+	}
+}
+
+func (c *coffeeService) GetCoffees() []entities.Coffee {
+	return c.CoffeeRepository.FindAll()
+}
+
+func (c *coffeeService) GetCoffeeById(id int) entities.Coffee {
+	return c.CoffeeRepository.FindById(uint(id))
+}
+
+func (c *coffeeService) GetCoffeeByName(coffeeName string) entities.Coffee {
+	return c.CoffeeRepository.FindByName(coffeeName)
+}
+
+func (c *coffeeService) CreateNewCoffee(newCoffee dto.CreateCoffee) entities.Coffee {
+	coffee := &entities.Coffee{
+		Name:        newCoffee.Name,
+		Description: newCoffee.Description,
+		Origin:      newCoffee.Origin,
+		Contents:    entities.MultiString(newCoffee.Contents),
+		Cost:        newCoffee.Cost,
+	}
+
+	return c.CoffeeRepository.Save(*coffee)
+}
+
+func (c *coffeeService) DeleteCoffee(id int) error {
+	coffee := c.CoffeeRepository.FindById(uint(id))
+
+	if coffee.ID == 0 {
+		return gorm.ErrRecordNotFound
+	}
+
+	c.CoffeeRepository.Delete(coffee)
+
+	return nil
+}
