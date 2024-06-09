99d880fb21095ec03f608bc3e5825a42a4ab5648
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-06-09 17:10:42 +0530

    creates payment handler and routes.

--- src/main/handlers/payHandler.go
+++ src/main/handlers/payHandler.go
@@ -1,6 +1,10 @@
 package handlers
 
 import (
+	"net/http"
+	"strconv"
+
+	"github.com/2k4sm/httpCoffee/src/main/dto"
 	"github.com/2k4sm/httpCoffee/src/main/services"
 	"github.com/gofiber/fiber/v2"
 )
@@ -24,6 +28,134 @@ func NewPayHandler(payService services.PayServiceInterface) PayHandlerInterface 
 	}
 }
 
+func (p *payHandler) GetAll(ctx *fiber.Ctx) error {
+	payments := p.PayService.GetAll()
 
+	if (len(payments)) == 0 {
+		return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"status":  http.StatusNotFound,
+			"message": "No payments found",
+		})
+	}
 
+	paymentsResponse := []dto.Payment{}
 
+	for _, payment := range payments {
+		paymentsResponse = append(paymentsResponse, dto.ParseFromPaymentEntity(payment))
+	}
+
+	return ctx.JSON(paymentsResponse)
+}
+
+func (p *payHandler) GetById(ctx *fiber.Ctx) error {
+	reqId := ctx.Params("paymentId")
+
+	reqIntId, err := strconv.Atoi(reqId)
+
+	if err != nil {
+		return ctx.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"status":  http.StatusBadRequest,
+			"message": err.Error(),
+		})
+	}
+
+	payment := p.PayService.GetById(reqIntId)
+
+	if payment.ID == 0 {
+		return ctx.Status(http.StatusNotFound).JSON(fiber.Map{
+			"status":  http.StatusNotFound,
+			"message": "Payment not found",
+		})
+	}
+
+	return ctx.JSON(dto.ParseFromPaymentEntity(payment))
+}
+
+func (p *payHandler) GetAllByUserId(ctx *fiber.Ctx) error {
+	reqId := ctx.Params("UserID")
+
+	reqIntId, err := strconv.Atoi(reqId)
+
+	if err != nil {
+		return ctx.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"status":  http.StatusBadRequest,
+			"message": err.Error(),
+		})
+	}
+
+	payment := p.PayService.GetById(reqIntId)
+
+	if payment.ID == 0 {
+		return ctx.Status(http.StatusNotFound).JSON(fiber.Map{
+			"status":  http.StatusNotFound,
+			"message": "Payment not found",
+		})
+	}
+
+	return ctx.JSON(dto.ParseFromPaymentEntity(payment))
+}
+
+func (p *payHandler) GetAllByHouseId(ctx *fiber.Ctx) error {
+	reqId := ctx.Params("CoffeeHouseID")
+
+	reqIntId, err := strconv.Atoi(reqId)
+
+	if err != nil {
+		return ctx.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"status":  http.StatusBadRequest,
+			"message": err.Error(),
+		})
+	}
+
+	payment := p.PayService.GetById(reqIntId)
+
+	if payment.ID == 0 {
+		return ctx.Status(http.StatusNotFound).JSON(fiber.Map{
+			"status":  http.StatusNotFound,
+			"message": "Payment not found",
+		})
+	}
+
+	return ctx.JSON(dto.ParseFromPaymentEntity(payment))
+}
+
+func (p *payHandler) CreatePayment(ctx *fiber.Ctx) error {
+
+	var newPayment dto.CreatePayment
+
+	if err := ctx.BodyParser(&newPayment); err != nil {
+		return ctx.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"status":  http.StatusBadRequest,
+			"message": err.Error(),
+		})
+	}
+
+	payment := p.PayService.CreatePayment(newPayment)
+
+	return ctx.JSON(dto.ParseFromPaymentEntity(payment))
+}
+
+func (p *payHandler) DeletePayment(ctx *fiber.Ctx) error {
+
+	reqId := ctx.Params("paymentId")
+
+	reqIntId, err := strconv.Atoi(reqId)
+
+	if err != nil {
+		return ctx.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"status":  http.StatusBadRequest,
+			"message": err.Error(),
+		})
+	}
+
+	payment, err := p.PayService.DeletePayment(reqIntId)
+
+	if err != nil {
+		return ctx.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"status":  http.StatusBadRequest,
+			"message": err.Error(),
+		})
+	}
+
+	return ctx.JSON(dto.ParseFromPaymentEntity(payment))
+}
---
+++ src/main/routes/payRoutes.go
@@ -0,0 +1,23 @@
+package routes
+
+import (
+	"github.com/2k4sm/httpCoffee/src/main/handlers"
+	"github.com/2k4sm/httpCoffee/src/main/repositories"
+	"github.com/2k4sm/httpCoffee/src/main/services"
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+func PaymentRoutes(router fiber.Router, db *gorm.DB) {
+	paymentRepository := repositories.NewPayRepository(db)
+	paymentService := services.NewPayService(paymentRepository)
+	paymentHandler := handlers.NewPayHandler(paymentService)
+
+	router.Get("/", paymentHandler.GetAll)
+	router.Get("/:paymentId", paymentHandler.GetById)
+	router.Get("/names/:CoffeeHouseID", paymentHandler.GetAllByHouseId)
+	router.Get("/names/:UserID", paymentHandler.GetAllByUserId)
+	router.Post("/", paymentHandler.CreatePayment)
+	router.Delete("/:paymentId", paymentHandler.DeletePayment)
+
+}
